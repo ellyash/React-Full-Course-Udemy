3529b0e4b6b448924d64bafb45db9759e81194c5
obcior <bartekpaciorek.bp@gmail.com>
2021-08-19 13:53:46 +0200

    final

---
+++ react-router-basics/src/App.js
@@ -0,0 +1,32 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to="/welcome" />
+					</Route>
+					<Route path="/welcome">
+						<Welcome />
+					</Route>
+					<Route path="/products" exact>
+						<Products />
+					</Route>
+					<Route path="/products/:productId">
+						<ProductDetail />
+					</Route>
+				</Switch>
+			</main>
+		</div>
+	);
+}
+
+export default App;
